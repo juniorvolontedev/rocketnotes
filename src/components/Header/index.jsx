ba14f0c31b0f43877822c50991bef4c97f132e38
Junior Volonte <juniorvolontedev@gmail.com>
2023-06-29 20:54:23 -0300

    imagem do usuário

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,19 @@
-import { Container } from './styles';
+import { Container, Profile } from './styles';
 
 export function Header() {
-  return <Container />;
+  return (
+    <Container>
+      <Profile>
+        <img
+          src="https://github.com/juniorvolontedev.png"
+          alt="Foto do Usuário"
+        />
+
+        <div>
+          <span>Bem-vindo</span>
+          <strong>Rodrigo Gonçalves</strong>
+        </div>
+      </Profile>
+    </Container>
+  );
 }
