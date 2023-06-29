dca921b60019e99a2db8e5ff75df282c3a2cf182
Junior Volonte <juniorvolontedev@gmail.com>
2023-06-29 17:41:17 -0300

    componente

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,10 +1,13 @@
 import { Container } from './styles.js';
 
+import { Button } from '../../components/Button';
+
 export function Details() {
   return (
     <Container>
       <h1>Hello Wolrd</h1>
       <span>Junior</span>
+      <Button />
     </Container>
   );
 }
