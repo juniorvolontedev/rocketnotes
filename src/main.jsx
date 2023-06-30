af045249b9d3ae36f1da14ef47232113d410af42
Junior Volonte <juniorvolontedev@gmail.com>
2023-06-30 15:39:29 -0300

    aplicando grid na home

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { ThemeProvider } from 'styled-components';
-import GlobaStyles from './styles/global';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "styled-components";
+import GlobaStyles from "./styles/global";
 
-import theme from './styles/theme';
+import theme from "./styles/theme";
 
-import { Details } from './pages/Details';
+import { Home } from "./pages/Home";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobaStyles />
-      <Details />
+      <Home />
     </ThemeProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
