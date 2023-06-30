180f6e0d054dcdba5b0108896ad75073e112c22a
Junior Volonte <juniorvolontedev@gmail.com>
2023-06-29 21:26:02 -0300

    componente button text

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -3,11 +3,13 @@ import { Header } from '../../components/Header/index.jsx';
 import { Section } from '../../components/Section';
 import { Tag } from '../../components/Tag';
 import { Button } from '../../components/Button';
+import { ButtonText } from '../../components/ButtonText';
 
 export function Details() {
   return (
     <Container>
       <Header />
+      <ButtonText title="Exluir nota" />
       <Section title="Links Úteis">
         <Links>
           <li>
@@ -15,7 +17,6 @@ export function Details() {
           </li>
         </Links>
       </Section>
-
       <Section title="Marcadores">
         <Tag title="express" />
         <Tag title="node" />
