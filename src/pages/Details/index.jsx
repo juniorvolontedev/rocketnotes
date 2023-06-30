b97064c5e89a1c9d280cbb71a0c33ecfe519ab30
Junior Volonte <juniorvolontedev@gmail.com>
2023-06-29 21:59:14 -0300

    finalizando detalhes

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,4 +1,4 @@
-import { Container, Links } from './styles.js';
+import { Container, Links, Content } from './styles.js';
 import { Header } from '../../components/Header/index.jsx';
 import { Section } from '../../components/Section';
 import { Tag } from '../../components/Tag';
@@ -9,19 +9,34 @@ export function Details() {
   return (
     <Container>
       <Header />
-      <ButtonText title="Exluir nota" />
-      <Section title="Links Úteis">
-        <Links>
-          <li>
-            <a href="#">https://rocketseat.com.br</a>
-          </li>
-        </Links>
-      </Section>
-      <Section title="Marcadores">
-        <Tag title="express" />
-        <Tag title="node" />
-      </Section>
-      <Button title="Voltar" />
+
+      <main>
+        <Content>
+          <ButtonText title="Exluir nota" />
+
+          <h1>Introdução ao React</h1>
+
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum,
+            asperiores adipisci. Ad incidunt cupiditate nesciunt voluptatum
+            eveniet accusamus, delectus iste fuga possimus quia? Obcaecati
+            tempora dolore suscipit. Maiores, cum hic.
+          </p>
+
+          <Section title="Links Úteis">
+            <Links>
+              <li>
+                <a href="#">https://rocketseat.com.br</a>
+              </li>
+            </Links>
+          </Section>
+          <Section title="Marcadores">
+            <Tag title="express" />
+            <Tag title="node" />
+          </Section>
+          <Button title="Voltar" />
+        </Content>
+      </main>
     </Container>
   );
 }
