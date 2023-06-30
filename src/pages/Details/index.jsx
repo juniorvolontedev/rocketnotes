14e45e889ea0ba23bc5299cf85e8fffe641d3bd2
Junior Volonte <juniorvolontedev@gmail.com>
2023-06-29 21:20:56 -0300

    componente tag

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,6 +1,7 @@
 import { Container, Links } from './styles.js';
 import { Header } from '../../components/Header/index.jsx';
 import { Section } from '../../components/Section';
+import { Tag } from '../../components/Tag';
 import { Button } from '../../components/Button';
 
 export function Details() {
@@ -13,6 +14,11 @@ export function Details() {
             <a href="#">https://rocketseat.com.br</a>
           </li>
         </Links>
+      </Section>
+
+      <Section title="Marcadores">
+        <Tag title="express" />
+        <Tag title="node" />
       </Section>
       <Button title="Voltar" />
     </Container>
