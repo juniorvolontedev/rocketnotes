96cce825944d18cd6085b88e3351e771be663eab
Junior Volonte <juniorvolontedev@gmail.com>
2023-06-29 21:13:48 -0300

    children

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,11 +1,19 @@
-import { Container } from './styles.js';
+import { Container, Links } from './styles.js';
 import { Header } from '../../components/Header/index.jsx';
+import { Section } from '../../components/Section';
 import { Button } from '../../components/Button';
 
 export function Details() {
   return (
     <Container>
       <Header />
+      <Section title="Links Úteis">
+        <Links>
+          <li>
+            <a href="#">https://rocketseat.com.br</a>
+          </li>
+        </Links>
+      </Section>
       <Button title="Voltar" />
     </Container>
   );
